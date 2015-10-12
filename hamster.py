08373c82da90bae1ade066984149fc78732e2889
Kevin DeLoach <kdeloach@azavea.com>
2015-10-12 15:25:44 -0400

    Round minutes to nearest 15 minute increment
    Time entries created on the same day are now grouped together and
    submitted as a single entry (per client/activity) with the minutes field
    rounded to the nearest 15 minute increment.

--- hamster.py
+++ hamster.py
@@ -11,7 +11,9 @@ import json
 import os.path
 import sqlite3
 import logging
+import itertools
 
+from collections import namedtuple
 from datetime import datetime, timedelta
 
 
@@ -20,7 +22,12 @@ log.addHandler(logging.StreamHandler(sys.stderr))
 log.setLevel(logging.DEBUG)
 
 
-DATE_FORMAT = '%m/%d/%Y %H:%M:%S'
+DATE_FORMAT = '%m/%d/%Y'
+DATETIME_FORMAT = '%m/%d/%Y %H:%M:%S'
+
+
+TimesheetRecord = namedtuple('TimesheetRecord', ['start_time', 'end_time',
+                             'customer', 'activity', 'comments', 'tags'])
 
 
 def fetch_rows(start_date, end_date):
@@ -43,38 +50,102 @@ def fetch_rows(start_date, end_date):
         where f.end_time is not null
             and f.start_time >= ? and f.end_time < ?
         group by f.id
+        order by a.name, c.name, f.start_time
     """
 
     with sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
-        for row in conn.execute(query, (start_date, end_date)):
-            start_time = row[0]
-            end_time = row[1]
-            minutes = int(math.ceil((end_time - start_time).seconds / 60))
-            comments = row[4] or ''
+        result = conn.execute(query, (start_date, end_date))
+        rows = (TimesheetRecord(*row) for row in result)
+        rows = squash_rows(rows)
+        rows = sorted(rows, key=lambda row: row.start_time)
+        return format_rows(rows)
 
-            # I find that it is most convenient to input time in hamster
-            # by using the following syntax:
-            #
-            # client name@software development, fixing some bugs... #billable
-            #
-            # Which means that "client name" maps to `activity` and
-            # "software development" maps to `category`.
-            #
-            # This is the reason why I need to swap `activity` and `category`
-            # in the final JSON output.
 
-            yield {
-                'start_time': start_time.strftime(DATE_FORMAT),
-                'end_time': end_time.strftime(DATE_FORMAT),
-                'minutes': minutes,
-                'comments': comments,
-                'customer': row[2],
-                'activity': row[3],
-                'tags': row[5],
-            }
+def squash_rows(rows):
+    keyfn = lambda row: (row.start_time.strftime(DATE_FORMAT),
+                         row.customer, row.activity)
+    grouped_rows = itertools.groupby(rows, keyfn)
+    for key, rows in grouped_rows:
+        yield reduce(combine_entries, rows)
+
+
+def combine_entries(acc, entry):
+    # Expand date range to include both entries.
+    start_time = min(acc.start_time, entry.start_time)
+    end_time = max(acc.end_time, entry.end_time)
+
+    # Separate multiple entry comments with semicolon.
+    comments = filter(None, [acc.comments, entry.comments])
+    comments = '; '.join(comments).strip()
+
+    # Use the longest tags field.
+    # TODO: Union all tags and remove duplicates (comma separated list)
+    tags = acc.tags if len(acc.tags) > len(entry.tags) else entry.tags
+
+    return TimesheetRecord(
+        start_time,
+        end_time,
+        acc.customer,
+        acc.activity,
+        comments,
+        tags
+    )
+
+
+def format_rows(rows):
+    for row in rows:
+        minutes = round_minutes(to_minutes(row.end_time - row.start_time))
+        yield {
+            'start_time': row.start_time.strftime(DATETIME_FORMAT),
+            'end_time': row.end_time.strftime(DATETIME_FORMAT),
+            'customer': row.customer,
+            'activity': row.activity,
+            'comments': row.comments or '',
+            'tags': row.tags or '',
+            'minutes': minutes,
+        }
+
+
+def to_minutes(delta):
+    """
+    Convert duration to minutes.
+    """
+    return int(math.ceil(delta.total_seconds() / 60))
+
+
+def round_minutes(minutes):
+    """
+    Round minutes to nearest 15 minute increment.
+    """
+    i = math.floor(minutes / 15)
+    under, over = i * 15, (i + 1) * 15
+    d1, d2 = abs(minutes - under), abs(minutes - over)
+    # Return the increment closest to the original value.
+    return over if d2 <= d1 else under
+
+
+def test():
+    today = datetime.now()
+    yesterday = datetime.now() - timedelta(days=1)
+    assert to_minutes(today - yesterday) == 24 * 60
+
+    assert to_minutes(timedelta(seconds=0)) == 0
+    assert to_minutes(timedelta(seconds=60)) == 1
+    assert to_minutes(timedelta(seconds=3600)) == 60
+
+    assert round_minutes(3) == 0
+    assert round_minutes(7) == 0
+    assert round_minutes(8) == 15
+    assert round_minutes(14) == 15
+    assert round_minutes(27) == 30
+    assert round_minutes(33) == 30
+    assert round_minutes(39) == 45
+    assert round_minutes(64) == 60
 
 
 def main():
+    test()
+
     yesterday = datetime.now() - timedelta(days=1)
     tomorrow = datetime.now() + timedelta(days=1)
 
@@ -84,9 +155,9 @@ def main():
     args = parser.parse_args()
 
     if isinstance(args.start_time, basestring):
-        args.start_time = datetime.strptime(args.start_time, DATE_FORMAT)
+        args.start_time = datetime.strptime(args.start_time, DATETIME_FORMAT)
     if isinstance(args.end_time, basestring):
-        args.end_time = datetime.strptime(args.end_time, DATE_FORMAT)
+        args.end_time = datetime.strptime(args.end_time, DATETIME_FORMAT)
 
     log.debug(args)
 
